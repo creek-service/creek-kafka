6b24a4fa5a310edc155f451ced3ff16b7c15042f
Big Andy <8012398+big-andy-coates@users.noreply.github.com>
2021-12-06 11:06:33 +0000

    Enable pulling of jars from GitHub

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     java
     jacoco
+    `maven-publish`
     id("com.github.spotbugs") version "4.7.0"                   // https://mvnrepository.com/artifact/com.github.spotbugs/spotbugs-gradle-plugin
     id("com.diffplug.spotless") version "6.0.0"                 // https://mvnrepository.com/artifact/com.diffplug.spotless/spotless-plugin-gradle
     id("pl.allegro.tech.build.axion-release") version "1.13.5"  // https://mvnrepository.com/artifact/pl.allegro.tech.build.axion-release/pl.allegro.tech.build.axion-release.gradle.plugin?repo=gradle-plugins
     id("com.github.kt3k.coveralls") version "2.12.0"            // https://plugins.gradle.org/plugin/com.github.kt3k.coveralls
-    `maven-publish`
 }
 
 project.version = scmVersion.version
@@ -29,6 +29,14 @@ allprojects {
 
     repositories {
         mavenCentral()
+
+        maven {
+            url = uri("https://maven.pkg.github.com/creek-service/*")
+            credentials {
+                username = "Creek-Bot-Token"
+                password = "\u0067hp_LtyvXrQZen3WlKenUhv21Mg6NG38jn0AO2YH"
+            }
+        }
     }
 }
 
