b56342a03ddc496bd2429118564b22545f9c9dbb
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2021-12-07 13:45:48 +0000

    Add kafka metadata types (#4)
    * Add kafka metadata types
    
    * Bean getter names

---
+++ kafka-metadata/src/main/java/org/creek/api/kafka/metadata/KafkaTopicConfig.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2021 Creek Contributors (https://github.com/creek-service)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.creek.api.kafka.metadata;
+
+
+import java.util.Map;
+import java.util.StringJoiner;
+
+/** Type for defining a Kafka topic's config */
+public interface KafkaTopicConfig {
+    /** @return topics partition count. */
+    int partitions();
+
+    /** @return any specific configs. If not supplied, cluster/env defaults will be used. */
+    default Map<String, String> config() {
+        return Map.of();
+    }
+
+    /** @return true if left and right are equivalent. */
+    static boolean matches(final KafkaTopicConfig left, final KafkaTopicConfig right) {
+        return left.partitions() == right.partitions() && left.config().equals(right.config());
+    }
+
+    /**
+     * Convert topic details to string.
+     *
+     * <p>Used when logging topic details.
+     * Avoids the need for every implementor of this type to define {@code toString).
+     *
+     * @param config the config to convert
+     * @return string representation
+     */
+    static String asString(final KafkaTopicConfig config) {
+        return new StringJoiner(", ", config.getClass().getSimpleName() + "[", "]")
+                .add("partitions=" + config.partitions())
+                .add("config=" + config.config())
+                .toString();
+    }
+}
