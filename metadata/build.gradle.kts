78be46fff7b84aeb4363b1ab09c18861bdec5198
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2022-03-16 13:14:25 +0000

    Add module-info.java (#22)

--- metadata/build.gradle.kts
+++ metadata/build.gradle.kts
@@ -18,6 +18,8 @@ plugins {
     `java-library`
 }
 
+val creekVersion : String by extra
+
 dependencies {
-    api("org.creek:creek-platform-metadata:+")
+    api("org.creek:creek-platform-metadata:$creekVersion")
 }
