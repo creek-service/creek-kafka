288c29cb62ed2129ee9b198a4df12bddd4328512
Big Andy <8012398+big-andy-coates@users.noreply.github.com>
2023-01-30 19:25:44 +0000

    Update copyright 2023

--- test-service/include/bin/run.sh
+++ test-service/include/bin/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Copyright 2022 Creek Contributors (https://github.com/creek-service)
+# Copyright 2022-2023 Creek Contributors (https://github.com/creek-service)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
