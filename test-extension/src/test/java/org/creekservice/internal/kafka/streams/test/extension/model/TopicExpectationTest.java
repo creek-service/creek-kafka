0c58023b90cfbf1e6536b3b2a0f88ab366132275
Big Andy <8012398+big-andy-coates@users.noreply.github.com>
2023-11-21 14:00:07 +0000

    Fix unit tests to make them run on Windows

--- test-extension/src/test/java/org/creekservice/internal/kafka/streams/test/extension/model/TopicExpectationTest.java
+++ test-extension/src/test/java/org/creekservice/internal/kafka/streams/test/extension/model/TopicExpectationTest.java
@@ -149,8 +149,7 @@ class TopicExpectationTest {
                         "Cannot construct instance of"
                             + " `org.creekservice.internal.kafka.streams.test.extension.model.TopicExpectation`,"
                             + " problem: Topic not set. Topic must be supplied either at the file"
-                            + " or record level. location: unknown"
-                                + lineSeparator()
+                            + " or record level. location: unknown\n"
                                 + " at [Source: (StringReader); line: 8, column: 13]"));
     }
 
