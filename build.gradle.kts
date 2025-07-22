65c1e6b286f59ec0106b42da400f5a90f1907ab6
Big Andy <8012398+big-andy-coates@users.noreply.github.com>
2025-07-22 10:32:12 +0100

    Update snapshot repo & a few versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,12 @@ plugins {
     id("org.creekservice.schema.json") version "0.4.2-SNAPSHOT" apply false
 }
 
+scmVersion {
+    versionCreator("simple")
+}
+
 project.version = scmVersion.version
+println("creekVersion: ${project.version}")
 
 allprojects {
     tasks.jar {
@@ -66,9 +71,9 @@ subprojects {
     }
 
     extra.apply {
-        set("creekVersion", "0.4.2-SNAPSHOT")
+        set("creekVersion", project.version)
         set("spotBugsVersion", "4.9.3")         // https://mvnrepository.com/artifact/com.github.spotbugs/spotbugs-annotations
-        set("jacksonVersion", "2.18.2")         // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-annotations
+        set("jacksonVersion", "2.19.1")         // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-annotations
         set("slf4jVersion", "2.0.17")            // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
         set("log4jVersion", "2.25.0")           // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core
         set("guavaVersion", "33.4.8-jre")         // https://mvnrepository.com/artifact/com.google.guava/guava
